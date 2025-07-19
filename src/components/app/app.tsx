59b8260d313de5d93dd1e867871276bd15453ff4
Vladislav Flinski <flinssski@gmail.com>
2025-07-19 14:38:48 +0300

    chore: set up configs

---
+++ src/components/app/app.tsx
@@ -0,0 +1,7 @@
+import styles from './app.module.scss'
+
+const { app } = styles
+
+export default function App() {
+	return <div className={app}>Hello!</div>
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	base: '/',
+	build: {
+		minify: true,
+		target: 'esnext',
+		outDir: 'dist',
+	},
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src/'),
+		},
+	},
 })
